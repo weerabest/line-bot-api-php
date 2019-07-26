fada55f664c96d37e05e556aa092f2e0e1bb3a0c
weerabest <53295570+weerabest@users.noreply.github.com>
2019-07-26 09:52:27 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "RY1yuFxqaJOqqGg"; //enter your web key
-		$SECRET = "8iAcnJfGMQeZ0fom44Vk9LzOp"; //enter your secret
+		$APPID= "BestIoT55/"; //enter your appid
+		$KEY = "1iVFH8TJbhssVrk"; //enter your web key
+		$SECRET = "XzlcZ4LpWEdSz0OdkYkdBSoPD"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
