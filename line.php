7c871f188c6257d3d7063d9f6307e41534d942df
weerabest <53295570+weerabest@users.noreply.github.com>
2019-07-26 09:34:52 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'NUyA4vpVSvoagOrkXThhGdVePUn3dyldQH0acP//D+aXGmgd0jrzS1AsMQ/NNONr7Rf15jx4UQY9NKUQdQHG3z3ZSctLqO7ZfXusd/XWIRIxiFj/T2RV8X+UnqUCWJD4DKJy0wdzunqsH5Zl1kupcQdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
